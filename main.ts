a945d2a812e7010d0c973fddc351150495c80788
cargau <57837914+cargau@users.noreply.github.com>
2021-03-30 17:37:29 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+basic.forever(function () {
+    basic.showString("Joyeux anniversaire!!!!")
+})
